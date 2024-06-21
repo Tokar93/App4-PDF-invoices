7ecce988745116258ddc28ed2886afa28f7a37a7
tokarski-to <tomasz.tokarski93@gmail.com>
2024-06-21 11:58:16 +0200

    Day 25: Generate separate PDFs for each animal with descriptions

---
+++ README.md
@@ -0,0 +1,2 @@
+# What is this project?
+The program takes information from txt files and convert them into PDF files with more user-friendly visual content
---
+++ main.py
@@ -0,0 +1,24 @@
+from fpdf import FPDF
+from pathlib import Path
+import glob
+
+animals = glob.glob('animals/*.txt')
+
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
+
+for animal in animals:
+    with open(animal, 'r') as file:
+        description = file.read()
+    animal_name = Path(animal).stem.title()
+    pdf.add_page()
+    pdf.set_font(size=16, style='B', family='Times')
+    pdf.cell(w=0, h=12, txt=animal_name, align='L',
+             ln=1, border=0)
+    pdf.set_font(size=10, family='Times' )
+    pdf.write(h=5, txt=description)
+    #pdf.cell(w=0, h=12, txt=description, align='L',
+             #ln=1, border=0)
+
+pdf.output('output.pdf')
+
